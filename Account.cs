584c43987f6c3d3277a1e7b085e9b050059c7ba5
hopemorehead <SplendidWonder@gmail.com>
2018-05-15 12:20:44 -0400

    MyFirstCommitForBankAccountRedo

---
+++ Account.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPersonalBankAccount
+{
+    // by default all classes are internal.Using this access modifier is  just a reminder to myself.
+
+    // Are class objects also data types?
+   internal class  Account
+    {
+        private static string accountBank;
+        //private by default
+        private string accountOwner;
+        private string accountType;
+        private int accountNumber;
+        private double accountBalance;
+
+
+        //because the parameter in the constructor is the same name and case as the instanse variable name hiding is enabled, 
+        // I don't want name hiding and I want class scope not just scope within the constructor....
+        //so I have to redirect the parameter back to the instance variable. The THIS keyword does this.
+        
+            
+
+            //constructors
+        static Account()
+        {
+         //   Account.accountBank = accountBank; this is redundent;
+        }
+
+        public Account()
+        {
+            this.accountOwner ="newuser";
+            this.accountType = "Savings";
+            this.accountNumber = 0987654321;
+
+        }
+        public Account(string accountOwner)
+        {
+            this.accountOwner = accountOwner;
+        }
+
+        public Account(string accountOwner,string accountType,int accountNumber)
+        {
+            this.accountOwner = accountOwner;
+            this.accountType = accountType;
+            this.accountNumber = accountNumber;
+        }
+
+        public Account(double accountBalance)
+        {
+            this.accountBalance = accountBalance;
+        }
+
+        //properties
+
+        public static string AccountBank
+        {
+
+            //get is used if someone is trying to retrieve info
+            get
+            {
+                return accountBank;
+            }
+            //set is used if someone is trying to change info
+            set
+            {
+                accountBank = value;
+            }
+        }
+
+
+        public string AccountOwner
+        {
+
+            //get is used if someone is trying to retrieve info
+            get
+            {
+                return accountOwner;
+            }
+            //set is used if someone is trying to change info
+            set
+            {
+                accountOwner = value;
+            }
+        }
+
+        public string AccountType
+        {
+
+            get
+            {
+                return accountType;
+            }
+            set
+            {
+                accountType = value;
+            }
+        }
+
+        public int AccountNumber
+        {
+
+            get
+            {
+                return accountNumber;
+            }
+            set
+            {
+                accountNumber = value;
+            }
+        }
+
+        public double AccountBalance
+        {
+
+            get
+            {
+             return accountBalance;
+             }
+            set
+            {
+                accountBalance = value;
+            }
+         }
+
+        public double withdrawal(double withdrawalBalance )
+        {
+            AccountBalance -= accountBalance;
+            withdrawalBalance = AccountBalance;
+            return AccountBalance;
+        }
+
+       public void deposit(double depositBalance)
+        {
+           
+          accountBalance= depositBalance += accountBalance;
+ 
+            Console.WriteLine(depositBalance);
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPersonalBankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Account customer1 = new Account("Hope_Morehead", "checking",1234567890);
+
+            Console.WriteLine($"This account belongs to {customer1.AccountOwner}with the account type {customer1.AccountType} and account number {customer1.AccountNumber} and account balance{customer1.AccountBalance}");
+            Account.AccountBank = "CharterOne";
+            Console.WriteLine(Account.AccountBank);
+            customer1.deposit(100);
+            Console.WriteLine(customer1.AccountBalance);
+            //  Console.WriteLine($"{customer1.AccountOwner}");
+                       
+            //when you make a new object you call the constructor
+            //get the class prepared intialize
+        }
+    }
+}
